615647e367beb7791de2042d4de7f3e26b5440f6
Harun R Rayhan <Harun R Rayhan@HarunRRayhanPC>
2016-11-05 16:01:21 +0600

    Full Validation done

--- classes/Validate.php
+++ classes/Validate.php
@@ -22,12 +22,35 @@ class Validate{
         foreach($items as $item => $rules) {
             foreach($rules as $rule => $rule_value) {
                 
-                $value = $source[$item];
+                $value = trim($source[$item]);
+                $item = escape($item);
                 
                 if($rule === 'required' && empty($value)){
                     $this->addError("{$item} is required");
-                } else {
-                    
+                } else if(!empty ($value)) {
+                    switch($rule) {
+                        case 'min':
+                            if(strlen($value) < $rule_value) {
+                                $this->addError("{$item} must be a minimum of {$rule_value} characters.");
+                            }
+                        break;
+                        case 'max':
+                            if(strlen($value) > $rule_value) {
+                                $this->addError("{$item} must be a maximum of {$rule_value} characters.");
+                            }
+                        break;
+                        case 'matches':
+                            if($value != $source[$rule_value]){
+                              $this->addError("{$rule_value} must match with {$item}")  ;
+                            }
+                        break;
+                        case 'unique':
+                            $check = $this->_db->get($rule_value, array($item, '=', $value));
+                            if($check->count()) {
+                                $this->addError("{$item} already exists.");
+                            }
+                        break;
+                    }
                 }
                 
             }
